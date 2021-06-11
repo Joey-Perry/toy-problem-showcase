ef0319bcb348dbe479e0243cc515966e73ff8cfd
Joey-Perry <Joeyap84@gmail.com>
2021-06-10 21:29:09 -0500

    Finished initial set of problems

--- src/components/Topics/Sum.js
+++ src/components/Topics/Sum.js
@@ -4,23 +4,47 @@ class Sum extends Component{
     constructor(props){
         super(props);
         this.state = {
-
+            number1: 0,
+            number2: 0,
+            sum: null,
         }
         this.handleClick = this.handleClick.bind(this);
-        this.handleChange = this.handleChange.bind(this);
+        this.updateFirstNumber = this.updateFirstNumber.bind(this);
+        this.updateSecondNumber = this.updateSecondNumber.bind(this);
+        // this.handleChange = this.handleChange.bind(this);
     }
-    solveProblem(){
+    solveProblem(num1, num2){
         /** Given two numbers, calculate the sum and display it. */
+        let number1 = num1 * 1;
+        let number2 = num2 * 1;
+        return number1 + number2;
     }
     handleClick(){
-        this.solveProblem();
+        const { number1, number2 } = this.state;
+        let result = this.solveProblem(number1, number2);
+        this.setState({ sum: result });
     }
-    handleChange(){
-        
+    updateFirstNumber(e){
+        this.setState({ number1: e.target.value })
+    }
+    updateSecondNumber(e){
+        this.setState({ number2: e.target.value })
     }
     render(){
         return(
-            <p> Sum Component</p>
+            <div className='puzzleBox sumPB'>
+                <h4>Sum</h4>
+                <input 
+                    onChange={this.updateFirstNumber}
+                    className='inputLine'/>
+                <input
+                    onChange={this.updateSecondNumber} 
+                    className='inputLine' />
+                <button 
+                    onClick={this.handleClick}
+                    className='confirmationButton'>Add</button>
+                <span className='resultsBox'>Result: {this.state.sum} </span>
+            </div>
         )
     }
 }
